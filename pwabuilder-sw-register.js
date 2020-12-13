720dc01be6cd532d6f1c13279360f3c763e4dc2e
Giulia Amaral <75877954+GiuliaAmaral@users.noreply.github.com>
2020-12-13 04:27:54 -0300

    pwa

---
+++ pwabuilder-sw-register.js
@@ -0,0 +1,49 @@
+go(); async function go(){
+    if (global.config.cacheForceClear) {
+        await serviceWorkerUnRegister();
+        window.location.reload();
+    } else {
+        await serviceWorkerRegister();
+    }
+}
+
+function serviceWorkerUnRegister() {
+    return new Promise((resolve, reject) => {
+        console.log("[PWA Builder] Cache desativado removendo serviceWorker");
+
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+            for (let registration of registrations) {
+                console.log('[PWA Builder] remoção do registro do Service Worker bem sucedida');
+                registration.unregister();
+                resolve();
+            }
+        }).catch(function (err) {
+            console.log('[PWA Builder] Falha na remoção do registro do Service Worker: ', err);
+            reject();
+        });
+    });
+}
+
+
+function serviceWorkerRegister() {
+    return new Promise((resolve) => {
+        console.log("[PWA Builder] Cache ativo pode usar a serviceWorker");
+
+        if ("serviceWorker" in navigator) {
+            if (navigator.serviceWorker.controller) {
+                console.log("[PWA Builder] funcionário de serviço ativo encontrado, não há necessidade de registrar");
+                resolve();
+            } else {
+                navigator.serviceWorker
+                    .register("pwabuilder-sw.js?global=" + encodeURIComponent(btoa(JSON.stringify(global))), {
+                        scope: "./"
+                    })
+                    .then(function (reg) {
+                        console.log("[PWA Builder] O trabalhador do serviço foi registrado para o escopo: " + reg.scope);
+                        global.config.cacheForceClear = false;
+                        resolve();
+                    });
+            }
+        }
+    });
+}
--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,111 @@
+let global = new URL(location).searchParams.get('global');
+global = JSON.parse(atob(global));
+
+
 // This is the service worker with the Cache-first network
-
 const CACHE = "pwabuilder-precache";
+const precacheFiles = [];
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+/* Add an array of files to precache for your app */
+if (global.config.precache) {
+  precacheFiles.push(
+    global.config.front.host + "/index.html",
+    global.config.front.host + "/index.js",
+    global.config.front.host + "/src/img/logotipo.png",
+    global.config.front.host + "/src/css/style.css",
+    global.config.front.host + "/src/css/fw/animate.css",
+    global.config.front.host + "/src/css/fw/fireworks.css",
+    global.config.front.host + "/src/css/fw/bootstrap.min.css",
+    global.config.front.host + "/src/css/fw/fontawesome-pro-5.12.0-web/css/all.min.css",
+    global.config.front.host + "/src/js/fun.js",
+    global.config.front.host + "/src/js/global.js",
+    global.config.front.host + "/src/js/fw/bootstrap.min.js",
+    global.config.front.host + "/src/js/fw/jquery.js",
+    global.config.front.host + "/src/js/fw/popper.js",
+    global.config.front.host + "/src/js/fw/progressbar.js",
+    global.config.front.host + "/modals/carregando.html",
+    global.config.front.host + "/cenas/acertou.html",
+    global.config.front.host + "/cenas/erro.html",
+    global.config.front.host + "/cenas/inicio.html",
+    global.config.front.host + "/cenas/barulho.html",
+    global.config.front.host + "/cenas/barulho.js",
+    global.config.front.host + "/cenas/vitoria.html"
+  );
+}
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Instalar processamento de eventos");
+
+  console.log("[PWA Builder] Ignorar a espera na instalação");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Armazenando em cache páginas durante a instalação");
+      return cache.addAll(precacheFiles);
+    })
+  );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE
-  })
-);
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Reivindicando clientes para a página atual");
+  event.waitUntil(self.clients.claim());
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
+
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            console.log("[PWA Builder] A solicitação de rede falhou e nenhum cache." + error);
+          });
+      }
+    )
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
